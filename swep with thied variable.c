608591629b6e5767aa792595a0d0337c7d43efda
srushtivjm <127848798+srushtivjm@users.noreply.github.com>
2023-03-14 17:20:33 +0530

    Add files via upload

---
+++ swep with thied variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	a=15;
+	b=17;
+	c=a;
+	a=b;
+	b=c;
+	printf("A=%d\n",a);
+	printf("B=%d",b);
+}
---
+++ swep without third variabla.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	a=7;
+	b=17;
+	a+=b;
+	b=a-b;
+	a-=b;
+	printf("A=%d\n",a);
+	printf("B=%d",b);
+}
